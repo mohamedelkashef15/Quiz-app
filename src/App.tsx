67135654a0ec5c9c02ee39e28cb0b3e35453e28f
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-08-31 15:58:19 +0300

    Loading questions from fake API

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
+import { useEffect, useReducer } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
+interface IState {
+  questions: string[];
+  status: string;
+}
+
+type Action = { type: "dataRecived"; payload: string[] } | { type: "dataFailed"; error: string };
+
+const initialState = {
+  questions: [],
+  // loading, ready, error
+  status: "loading",
+};
+
+function reducer(state: IState, action: Action) {
+  switch (action.type) {
+    case "dataRecived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Wrong Action");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    async function fetchData() {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+        const data = await res.json();
+        dispatch({ type: "dataRecived", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed", error: (err as Error).message });
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="app">
       <Header />
